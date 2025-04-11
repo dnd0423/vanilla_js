d4d744f5c6ab1dbf7c5f4724b3985183dfe27adf
SEUNG <dnd0423@gmail.com>
2025-04-11 15:45:41 +0900

    계산기 프로그램 완성

--- 2/script.js
+++ 2/script.js
@@ -2,6 +2,10 @@ const screen = document.getElementById("screen");
 
 const buttons = document.querySelectorAll("button");
 
+const resultButton = document.getElementById("resultButton");
+
+const resetButton = document.getElementById("resetButton");
+
 let currentInput = "";
 
 const operatorRegex = /^(\d+|\*\*|[+\-*/])$/;
@@ -33,3 +37,88 @@ function calc(operator, numbers){
   }
 
 }
+
+function handleButtonClick(event){
+  event.preventDefault();
+
+  const buttonText = event.target.innerText;
+
+  if(numberRegex.test(buttonText) == true){
+    appendToScreen(buttonText);
+  }else if(operatorRegex.test(buttonText) == true){
+    appendToScreen(buttonText);
+  }
+
+}
+
+function initButtonListeners(){
+  buttons.forEach((button) => {
+    button.addEventListener("click", handleButtonClick);
+  })
+}
+
+function initSingleResultButtonListeners(){
+  resultButton.addEventListener("click", resultClick);
+}
+
+function initSingleResetButtonListeners(){
+  resetButton.addEventListener("click", resetClick);
+}
+
+function handleResultClick(){
+  const screenValue = screen.value;
+
+  if(screenValue.includes("+")){
+    const [num1, num2] = screenValue.split("+");
+
+    screen.value = calc("+", [num1, num2]);
+
+    return screen.value;
+  }else if(screenValue.includes("-")){
+    const [num1, num2] = screenValue.split("-");
+
+    screen.value = calc("-", [num1, num2]);
+
+    return screen.value;
+  }else if(screenValue.includes("*")){
+    const [num1, num2] = screenValue.split("*");
+
+    screen.value = calc("*", [num1, num2]);
+
+    return screen.value;
+  }else if(screenValue.includes("/")){
+    const [num1, num2] = screenValue.split("/");
+
+    screen.value = calc("/", [num1, num2]);
+
+    return screen.value;
+  }
+}
+
+function resultClick(event){
+  event.preventDefault();
+
+  const buttonText = event.target.innerText;
+
+  if(buttonText === "="){
+    screen.value = handleResultClick();
+  }
+
+}
+
+function resetClick(event){
+  event.preventDefault();
+
+  const buttonText = event.target.innerText;
+
+  if(buttonText === "c" ){
+    screen.value = "";
+  }
+}
+
+
+
+initButtonListeners();
+initSingleResultButtonListeners();
+initSingleResetButtonListeners();
+
